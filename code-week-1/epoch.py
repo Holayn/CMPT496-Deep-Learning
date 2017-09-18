ca9360620ecb3b7ff7b8257ea95d20f5d4c17456
Holayn <kwwong15@punahou.edu>
2017-09-18 17:08:45 -0400

    Added lab code and notes

---
+++ code-week-1/epoch.py
@@ -0,0 +1,87 @@
+# Number of Epochs in our training
+numEpochs = 7000
+
+# Defining our learning rate iterations (decay)
+learningRate = tf.train.exponential_decay(learning_rate=0.0008,
+                                          global_step= 1,
+                                          decay_steps=trainX.shape[0],
+                                          decay_rate= 0.95,
+                                          staircase=True)
+
+#Defining our cost function - Squared Mean Error
+cost_OP = tf.nn.l2_loss(activation_OP-yGold, name="squared_error_cost")
+
+#Defining our Gradient Descent
+training_OP = tf.train.GradientDescentOptimizer(learningRate).minimize(cost_OP)
+
+# Create a tensorflow session
+sess = tf.Session()
+
+# Initialize our weights and biases variables.
+init_OP = tf.global_variables_initializer()
+
+# Initialize all tensorflow variables
+sess.run(init_OP)
+
+# argmax(activation_OP, 1) returns the label with the most probability
+# argmax(yGold, 1) is the correct label
+correct_predictions_OP = tf.equal(tf.argmax(activation_OP,1),tf.argmax(yGold,1))
+
+# If every false prediction is 0 and every true prediction is 1, the average returns us the accuracy
+accuracy_OP = tf.reduce_mean(tf.cast(correct_predictions_OP, "float"))
+
+# Summary op for regression output
+activation_summary_OP = tf.summary.histogram("output", activation_OP)
+
+# Summary op for accuracy
+accuracy_summary_OP = tf.summary.scalar("accuracy", accuracy_OP)
+
+# Summary op for cost
+cost_summary_OP = tf.summary.scalar("cost", cost_OP)
+
+# Summary ops to check how variables (W, b) are updating after each iteration
+weightSummary = tf.summary.histogram("weights", weights.eval(session=sess))
+biasSummary = tf.summary.histogram("biases", bias.eval(session=sess))
+
+# Merge all summaries
+merged = tf.summary.merge([activation_summary_OP, accuracy_summary_OP, cost_summary_OP, weightSummary, biasSummary])
+
+# Summary writer
+writer = tf.summary.FileWriter("summary_logs", sess.graph)
+
+# Initialize reporting variables
+cost = 0
+diff = 1
+epoch_values = []
+accuracy_values = []
+cost_values = []
+
+# Training epochs
+for i in range(numEpochs):
+    if i > 1 and diff < .0001:
+        print("change in cost %g; convergence."%diff)
+        break
+    else:
+        # Run training step
+        step = sess.run(training_OP, feed_dict={X: trainX, yGold: trainY})
+        # Report occasional stats
+        if i % 10 == 0:
+            # Add epoch to epoch_values
+            epoch_values.append(i)
+            # Generate accuracy stats on test data
+            train_accuracy, newCost = sess.run([accuracy_OP, cost_OP], feed_dict={X: trainX, yGold: trainY})
+            # Add accuracy to live graphing variable
+            accuracy_values.append(train_accuracy)
+            # Add cost to live graphing variable
+            cost_values.append(newCost)
+            # Re-assign values for variables
+            diff = abs(newCost - cost)
+            cost = newCost
+            #generate print statements
+            print("step %d, training accuracy %g, cost %g, change in cost %g"%(i, train_accuracy, newCost, diff))
+
+
+# How well do we perform on held-out test data?
+print("final accuracy on test set: %s" %str(sess.run(accuracy_OP, 
+                                                     feed_dict={X: testX, 
+                                                                yGold: testY})))
