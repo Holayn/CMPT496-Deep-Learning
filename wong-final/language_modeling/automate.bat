6d85d34b8e920e1d5590c5d6f9c8f23dc1aec916
Kai <kwwong1597@gmail.com>
2017-12-09 11:51:31 -0500

    more auto

--- wong-final/language_modeling/automate.bat
+++ wong-final/language_modeling/automate.bat
@@ -1,8 +1,6 @@
-python lanmod.py 2.0 0.25 30 800 >D:\final_2_25_30_800
-python lanmod.py 2.0 0.25 30 1000 >D:\final_2_25_30_1000
-
-python lanmod.py 2.5 0.25 30 800 >D:\final_25_25_30_800
-python lanmod.py 2.5 0.25 30 1000 >D:\final_25_25_30_1000
-
-python lanmod.py 3.0 0.25 30 800 >D:\final_3_25_30_800
-python lanmod.py 3.0 0.25 30 1000 >D:\final_3_25_30_1000
+python lanmod.py 3.0 0.25 30 200 >D:\final_3_25_30_200
+python lanmod.py 2.0 0.25 30 200 >D:\final_2_25_30_200
+python lanmod.py 2.5 0.25 30 200 >D:\final_25_25_30_200
+python lanmod.py 4.0 0.25 30 200 >D:\final_4_25_30_200
+python lanmod.py 3.0 0.1 30 200 >D:\final_3_1_30_200
+python lanmod.py 3.0 0.3 30 200 >D:\final_3_3_30_200
