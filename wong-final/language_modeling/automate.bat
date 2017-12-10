afb1a1742732524920e00c65a1d869c56e614489
Kai <kwwong1597@gmail.com>
2017-12-10 02:05:24 -0500

    added more tests

--- wong-final/language_modeling/automate.bat
+++ wong-final/language_modeling/automate.bat
@@ -1,11 +1,16 @@
 python lanmod.py 1.0 0.25 20 200 .9 18 1.0 >D:\final_1_25_30_200_9_18_1
 python lanmod.py 1.0 0.25 20 200 .9 23 1.0 >D:\final_1_25_30_200_9_23_1
 python lanmod.py 1.0 0.25 20 200 .9 28 1.0 >D:\final_1_25_30_200_9_28_1
+python lanmod.py 1.0 0.25 20 200 .9 33 1.0 >D:\final_1_25_30_200_9_33_1
 
 python lanmod.py 1.0 0.25 20 200 .9 18 2.5 >D:\final_1_25_30_200_9_18_25
 python lanmod.py 1.0 0.25 20 200 .9 23 2.5 >D:\final_1_25_30_200_9_23_25
 python lanmod.py 1.0 0.25 20 200 .9 28 2.5 >D:\final_1_25_30_200_9_28_25
+python lanmod.py 1.0 0.25 20 200 .9 28 2.5 >D:\final_1_25_30_200_9_28_25
+python lanmod.py 1.0 0.25 20 200 .9 33 2.5 >D:\final_1_25_30_200_9_33_25
 
 python lanmod.py 1.0 0.25 20 200 .9 18 5.0 >D:\final_1_25_30_200_9_18_5
 python lanmod.py 1.0 0.25 20 200 .9 23 5.0 >D:\final_1_25_30_200_9_23_5
 python lanmod.py 1.0 0.25 20 200 .9 28 5.0 >D:\final_1_25_30_200_9_28_5
+python lanmod.py 1.0 0.25 20 200 .9 28 5.0 >D:\final_1_25_30_200_9_28_5
+python lanmod.py 1.0 0.25 20 200 .9 33 5.0 >D:\final_1_25_30_200_9_33_5
