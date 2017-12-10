2945c46df04f5e210b9ae6f65cb502efd4b50b3a
Kai <kwwong1597@gmail.com>
2017-12-10 15:05:40 -0500

    more tests and final tests

--- wong-final/language_modeling/automate.bat
+++ wong-final/language_modeling/automate.bat
@@ -1,14 +1,6 @@
-python lanmod.py 1.0 0.25 20 200 .9 18 1.0 >D:\final_1_25_30_200_9_18_1
-python lanmod.py 1.0 0.25 20 200 .9 23 1.0 >D:\final_1_25_30_200_9_23_1
-python lanmod.py 1.0 0.25 20 200 .9 28 1.0 >D:\final_1_25_30_200_9_28_1
-python lanmod.py 1.0 0.25 20 200 .9 33 1.0 >D:\final_1_25_30_200_9_33_1
+python lanmod.py 2.0 0.25 20 200 .9 18 1.0 >D:\final_2_25_30_200_9_18_1
+python lanmod.py 2.0 0.25 20 400 .9 18 1.0 >D:\final_2_25_30_400_9_18_1
+python lanmod.py 2.0 0.25 20 500 .9 18 1.0 >D:\final_2_25_30_500_9_18_1
 
-python lanmod.py 1.0 0.25 20 200 .9 18 2.5 >D:\final_1_25_30_200_9_18_25
-python lanmod.py 1.0 0.25 20 200 .9 23 2.5 >D:\final_1_25_30_200_9_23_25
-python lanmod.py 1.0 0.25 20 200 .9 28 2.5 >D:\final_1_25_30_200_9_28_25
-python lanmod.py 1.0 0.25 20 200 .9 33 2.5 >D:\final_1_25_30_200_9_33_25
-
-python lanmod.py 1.0 0.25 20 200 .9 18 5.0 >D:\final_1_25_30_200_9_18_5
-python lanmod.py 1.0 0.25 20 200 .9 23 5.0 >D:\final_1_25_30_200_9_23_5
-python lanmod.py 1.0 0.25 20 200 .9 28 5.0 >D:\final_1_25_30_200_9_28_5
-python lanmod.py 1.0 0.25 20 200 .9 33 5.0 >D:\final_1_25_30_200_9_33_5
+python lanmod_final.py 1.0 0.25 30 200 .9 18 1.0 >D:\final_final_1_25_30_200_9_18_1
+python lanmod_final.py 2.0 0.25 30 500 .9 18 1.0 >D:\final_final_2_25_30_200_9_18_1
